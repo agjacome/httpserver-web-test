3220b9a8100a814d6a04f47e75d435d232305054
Alberto G. Jácome <agjacome@gmail.com>
2016-02-02 17:59:08 +0100

    Add HttpMethod interface and Standard HttpMethods
    A new interface for HTTP Methods has been created, with means to retrieve the
    method name and whether if it is idempotent and/or safe. Another method to
    match on HttpExchange objects has also been default-implemented in this
    interface.
    
    StandardHttpMethods is an enum implementing this HttpMethod API for al standard
    methods defined in https://www.iana.org/assignments/http-methods/http-methods.xhtml.

---
+++ src/main/java/com/github/agjacome/httpserver/server/HttpMethod.java
@@ -0,0 +1,94 @@
+package com.github.agjacome.httpserver.server;
+
+import java.util.Optional;
+
+import com.sun.net.httpserver.HttpExchange;
+
+import static java.util.Objects.requireNonNull;
+
+public interface HttpMethod {
+
+    public String getName();
+
+    public boolean isIdempotent();
+
+    public boolean isSafe();
+
+    public default boolean matches(final HttpExchange exchange) {
+        return exchange.getRequestMethod().equals(getName());
+    }
+
+
+    public static enum StandardHttpMethod implements HttpMethod {
+
+        // https://www.iana.org/assignments/http-methods/http-methods.xhtml
+        ACL(false, true),
+        BASELINE_CONTROL(false, true),
+        BIND(false, true),
+        CHECKIN(false, true),
+        CHECKOUT(false, true),
+        CONNECT(false, false),
+        COPY(false, true),
+        DELETE(false, true),
+        GET(true, true),
+        HEAD(true, true),
+        LABEL(false, true),
+        LINK(false, true),
+        LOCK(false, false),
+        MERGE(false, true),
+        MKACTIVITY(false, true),
+        MKCALENDAR(false, true),
+        MKCOL(false, true),
+        MKREDIRECTREF(false, true),
+        MKWORKSPACE(false, true),
+        MOVE(false, true),
+        OPTIONS(true, true),
+        ORDERPATCH(false, true),
+        PATCH(false, false),
+        POST(false, false),
+        PRI(true, true),
+        PROPFIND(true, true),
+        PROPPATCH(false, true),
+        PUT(false, true),
+        REBIND(false, true),
+        REPORT(true, true),
+        SEARCH(true, true),
+        TRACE(true, true),
+        UNBIND(false, true),
+        UNCHECKOUT(false, true),
+        UNLINK(false, true),
+        UNLOCK(false, true),
+        UPDATE(false, true),
+        UPDATEREDIRECTREF(false, true),
+        VERSION_CONTROL(false, true);
+
+        private final boolean safe;
+        private final boolean idempotent;
+
+        public static Optional<HttpMethod> of(final String name) {
+            final String method = requireNonNull(name).replace('-', '_');
+
+            try {
+                return Optional.of(StandardHttpMethod.valueOf(method));
+            } catch (final IllegalArgumentException iae) {
+                return Optional.empty();
+            }
+        }
+
+        private StandardHttpMethod(final boolean safe, final boolean idempotent) {
+            this.safe       = safe;
+            this.idempotent = idempotent;
+        }
+
+        @Override
+        public String getName() {
+            return name().replace('_', '-');
+        }
+
+        @Override public boolean isIdempotent() { return idempotent; }
+        @Override public boolean isSafe()       { return safe;       }
+
+    }
+
+}
+
---
+++ src/test/java/com/github/agjacome/httpserver/server/StandardHttpMethodTest.java
@@ -0,0 +1,87 @@
+package com.github.agjacome.httpserver.server;
+
+import java.util.Set;
+
+import org.junit.contrib.theories.DataPoints;
+import org.junit.contrib.theories.Theories;
+import org.junit.contrib.theories.Theory;
+import org.junit.runner.RunWith;
+
+import com.github.agjacome.httpserver.server.HttpMethod.StandardHttpMethod;
+import com.github.agjacome.httpserver.util.Arrays;
+import com.pholser.junit.quickcheck.ForAll;
+import com.pholser.junit.quickcheck.generator.ValuesOf;
+import com.sun.net.httpserver.HttpExchange;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assume.assumeFalse;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@RunWith(Theories.class)
+public class StandardHttpMethodTest {
+
+    @DataPoints
+    public static final Set<String> standardMethods = Arrays.asSet(
+        "ACL", "BASELINE-CONTROL", "BIND", "CHECKIN", "CHECKOUT", "CONNECT",
+        "COPY", "DELETE", "GET", "HEAD", "LABEL", "LINK", "LOCK", "MERGE",
+        "MKACTIVITY", "MKCALENDAR", "MKCOL", "MKREDIRECTREF", "MKWORKSPACE",
+        "MOVE", "OPTIONS", "ORDERPATCH", "PATCH", "POST", "PRI", "PROPFIND",
+        "PROPPATCH", "PUT", "REBIND", "REPORT", "SEARCH", "TRACE", "UNBIND",
+        "UNCHECKOUT", "UNLINK", "UNLOCK", "UPDATE", "UPDATEREDIRECTREF",
+        "VERSION-CONTROL"
+    );
+
+    @Theory
+    public void of_must_return_empty_if_method_name_is_not_standard(
+        @ForAll final String string
+    ) {
+        assumeFalse(standardMethods.contains(string));
+        assertThat(StandardHttpMethod.of(string)).isEmpty();
+    }
+
+    @Theory
+    public void of_must_return_present_if_method_name_is_standard(
+        final String method
+    ) {
+        assertThat(StandardHttpMethod.of(method)).isPresent();
+    }
+
+    @Theory
+    public void get_name_must_return_hyphenated_name(
+        @ForAll @ValuesOf final StandardHttpMethod method
+    ) {
+        assertThat(method.getName()).doesNotContain("_").isIn(standardMethods);
+    }
+
+    @Theory
+    public void matches_must_match_on_correct_request_method(
+        @ForAll @ValuesOf final StandardHttpMethod method
+    ) {
+        final HttpExchange mockedExchange = mock(HttpExchange.class);
+
+        when(mockedExchange.getRequestMethod()).thenReturn(method.getName());
+
+        assertThat(method.matches(mockedExchange)).isTrue();
+
+        verify(mockedExchange).getRequestMethod();
+    }
+
+    @Theory
+    public void matches_must_not_match_on_incorrect_request_method(
+        @ForAll(sampleSize = 10) final String string,
+        @ForAll @ValuesOf final StandardHttpMethod method
+    ) {
+        assumeFalse(method.getName().equals(string));
+
+        final HttpExchange mockedExchange = mock(HttpExchange.class);
+
+        when(mockedExchange.getRequestMethod()).thenReturn(string);
+
+        assertThat(method.matches(mockedExchange)).isFalse();
+
+        verify(mockedExchange).getRequestMethod();
+    }
+
+}
